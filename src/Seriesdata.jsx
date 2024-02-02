9762284e3c5ab3987b594520a0035cdcc86e1798
Shweta <khandagaleshweta2002@gmail.com>
2024-02-02 18:20:59 +0530

    Commit2

---
+++ src/Seriesdata.jsx
@@ -0,0 +1,35 @@
+
+const Seriesdata=[
+        { imgsrc : "https://image.tmdb.org/t/p/original/63jwFUMWkHeFOldEVfSp3VE0GNn.jpg",
+          title : " A mxplayer Original Series ",
+          sname : 'Daydreamer',
+          link : "https://www.mxplayer.in/show/watch-daydreamer-hindi-dubbed-series-online-8f484e1869cc333bce98594fa2da59e4",
+        },
+        {
+          imgsrc : "https://image.tmdb.org/t/p/original/5e2owvs9TWVsuIacTFxJGPp6KVW.jpg",
+          title : " A mxplayer Original Series",
+          sname : " My girlfriend is an Alian ",
+          link : "https://www.mxplayer.in/show/watch-my-girlfriend-is-an-alien-hindi-dubbed-series-online-0930fb169d387f3cd0bdc0f920814a8b",
+        },
+        {
+          imgsrc : "https://image.tmdb.org/t/p/original/iuX0TRkczgbkd1tcJD01lvTWQRl.jpg",
+          title : " A mxplayer original series ",
+          sname : " Put Your Head On My Shoulder ",
+          link : "https://www.mxplayer.in/show/watch-put-your-head-on-my-shoulder-hindi-dubbed-series-online-c6382211316cb17d1757d82cf9605e4a",
+        },
+        {
+          imgsrc : "https://image.tmdb.org/t/p/original/77PP7SfgONe8AgdTHXg3nHwExQo.jpg",
+          title :  " A mxplayer original series ",
+          sname :  " RichMan ",
+          link : "https://www.mxplayer.in/show/watch-rich-man-hindi-dubbed/seasons/season-1-5ad29c556cbd0fbd1046e32876448488",
+        },
+        {
+          imgsrc : "https://image.tmdb.org/t/p/original/gjt8jB36i5YYg79Fi10ez6IcLgk.jpg",
+          title : " A mxplayer original series ",
+          sname : " Our Story ",
+          link : "https://www.mxplayer.in/show/watch-our-story-hindi-dubbed-series-online-8c042e67dbcb727a80953d0733519c95"
+        }
+  
+];
+
+export default Seriesdata;
--- src/index.js
+++ src/index.js
@@ -2,30 +2,36 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Card from "./Cards";
 import './index.css';
+import './Seriesdata';
+import Seriesdata from "./Seriesdata";
 
 ReactDOM.render(
   <>
-    <Card imgsrc="https://image.tmdb.org/t/p/original/63jwFUMWkHeFOldEVfSp3VE0GNn.jpg" 
-          title=" A mxplayer Original Series "
-          sname = 'Daydreamer'
-          link = "https://www.mxplayer.in/show/watch-daydreamer-hindi-dubbed-series-online-8f484e1869cc333bce98594fa2da59e4"/> 
-    <Card imgsrc = "https://image.tmdb.org/t/p/original/5e2owvs9TWVsuIacTFxJGPp6KVW.jpg"
-          title = " A mxplayer Original Series"
-          sname = " My girlfriend is an Alian "
-          link = "https://www.mxplayer.in/show/watch-my-girlfriend-is-an-alien-hindi-dubbed-series-online-0930fb169d387f3cd0bdc0f920814a8b"/>
-    <Card imgsrc = "https://image.tmdb.org/t/p/original/iuX0TRkczgbkd1tcJD01lvTWQRl.jpg"
-          title = " A mxplayer original series "
-          sname = " Put Your Head On My Shoulder "
-          link = "https://www.mxplayer.in/show/watch-put-your-head-on-my-shoulder-hindi-dubbed-series-online-c6382211316cb17d1757d82cf9605e4a"/>
-    <Card imgsrc="https://image.tmdb.org/t/p/original/77PP7SfgONe8AgdTHXg3nHwExQo.jpg"
-          title = " A mxplayer original series "
-          sname = " RichMan "
-          link = "https://www.mxplayer.in/show/watch-rich-man-hindi-dubbed/seasons/season-1-5ad29c556cbd0fbd1046e32876448488"/>
-
-    <Card imgsrc = "https://image.tmdb.org/t/p/original/gjt8jB36i5YYg79Fi10ez6IcLgk.jpg"
-          title = " A mxplayer original series "
-          sname = " Our Story "
-          link = "https://www.mxplayer.in/show/watch-our-story-hindi-dubbed-series-online-8c042e67dbcb727a80953d0733519c95" />
+    <Card imgsrc={Seriesdata[0].imgsrc} 
+          title= {Seriesdata[0].title}
+          sname = {Seriesdata[0].sname}
+          link = {Seriesdata[0].link}
+      /> 
+      <Card imgsrc={Seriesdata[1].imgsrc} 
+          title= {Seriesdata[1].title}
+          sname = {Seriesdata[1].sname}
+          link = {Seriesdata[1].link}
+      /> 
+      <Card imgsrc={Seriesdata[2].imgsrc} 
+          title= {Seriesdata[2].title}
+          sname = {Seriesdata[2].sname}
+          link = {Seriesdata[2].link}
+      /> 
+      <Card imgsrc={Seriesdata[3].imgsrc} 
+          title= {Seriesdata[3].title}
+          sname = {Seriesdata[3].sname}
+          link = {Seriesdata[3].link}
+      /> 
+      <Card imgsrc={Seriesdata[4].imgsrc} 
+          title= {Seriesdata[4].title}
+          sname = {Seriesdata[4].sname}
+          link = {Seriesdata[4].link}
+      /> 
   </>
   ,document.getElementById('root')
 );
