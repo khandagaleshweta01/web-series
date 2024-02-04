d1050594c6102813c92e03f31d5e4676bcac16c0
Shweta <khandagaleshweta2002@gmail.com>
2024-02-04 20:26:07 +0530

    code simplified using map()

--- src/index.js
+++ src/index.js
@@ -5,33 +5,18 @@ import './index.css';
 import './Seriesdata';
 import Seriesdata from "./Seriesdata";
 
+function ncard(val){
+    return(
+        <Card imgsrc={val.imgsrc} 
+        title= {val.title}
+        sname = {val.sname}
+        link = {val.link}
+    />         
+    )
+}
 ReactDOM.render(
-  <>
-    <Card imgsrc={Seriesdata[0].imgsrc} 
-          title= {Seriesdata[0].title}
-          sname = {Seriesdata[0].sname}
-          link = {Seriesdata[0].link}
-      /> 
-      <Card imgsrc={Seriesdata[1].imgsrc} 
-          title= {Seriesdata[1].title}
-          sname = {Seriesdata[1].sname}
-          link = {Seriesdata[1].link}
-      /> 
-      <Card imgsrc={Seriesdata[2].imgsrc} 
-          title= {Seriesdata[2].title}
-          sname = {Seriesdata[2].sname}
-          link = {Seriesdata[2].link}
-      /> 
-      <Card imgsrc={Seriesdata[3].imgsrc} 
-          title= {Seriesdata[3].title}
-          sname = {Seriesdata[3].sname}
-          link = {Seriesdata[3].link}
-      /> 
-      <Card imgsrc={Seriesdata[4].imgsrc} 
-          title= {Seriesdata[4].title}
-          sname = {Seriesdata[4].sname}
-          link = {Seriesdata[4].link}
-      /> 
+  <>   
+    {Seriesdata.map(ncard)};
   </>
   ,document.getElementById('root')
 );
